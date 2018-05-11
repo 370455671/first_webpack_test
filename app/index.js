34048988ac484d05451ca5ce0a62e2936b5f6e2f
370455671 <370455671@qq.com>
2018-05-11 23:13:08 +0800

    first webpack demo

---
+++ app/index.js
@@ -0,0 +1,23 @@
+// import _ from 'lodash'
+// function component(){
+//     var element = document.createElement('div')
+//     /*lodashi is required for the next line to work*/
+//     element.innerHTML = _.join(['Hello World'],' ')
+//     return element
+// }
+// document.body.appendChild(component())
+
+
+
+// import _ from 'lodash';
+// function component () {
+//     var element = document.createElement('div');
+  
+//     /* lodash is required for the next line to work */
+//     element.innerHTML = _.join(['Hello','webpack'], ' ');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+console.log(1)
